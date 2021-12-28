a2cfc8395874a3922ea971e626fab55a9db8c4b8
Kyeongjun <v49011591@gmail.com>
2021-12-28 10:03:13 +0900

    PasswordEncoder를 이용한 스프링 시큐리티 커스터 마이징

--- src/main/java/com/kkj/study/springsecurity/account/AccountService.java
+++ src/main/java/com/kkj/study/springsecurity/account/AccountService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,9 @@ public class AccountService implements UserDetailsService {
     //TODO {noop}123 패스워드 인코딩
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,7 +34,7 @@ public class AccountService implements UserDetailsService {
     }
 
     public Account createNew(Account account) {
-        account.setPassword("{noop}" + account.getPassword());
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return this.accountRepository.save(account);
     }
 }
