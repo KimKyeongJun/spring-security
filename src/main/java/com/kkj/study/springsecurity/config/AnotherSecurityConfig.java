c9764247066ac223e634c722a7c433c1bd4b29db
Kyeongjun <v49011591@gmail.com>
2021-12-30 14:51:54 +0900

    [권한 관리] 1. AccessDecisionManager를 커스터마이징 2. AccessDecisionManager의 voter가 사용하는 ExpressionHandler 커스터마이징

--- src/main/java/com/kkj/study/springsecurity/config/AnotherSecurityConfig.java
+++ src/main/java/com/kkj/study/springsecurity/config/AnotherSecurityConfig.java
@@ -1,20 +1,20 @@
-package com.kkj.study.springsecurity.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@Order(Ordered.LOWEST_PRECEDENCE -100)
-public class AnotherSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll(); // 기타 다른 url은 인증을 해야한다.
-        http.formLogin();
-        http.httpBasic();
-    }
-}
+//package com.kkj.study.springsecurity.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@Order(Ordered.LOWEST_PRECEDENCE -100)
+//public class AnotherSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll(); // 기타 다른 url은 인증을 해야한다.
+//        http.formLogin();
+//        http.httpBasic();
+//    }
+//}
--- src/main/java/com/kkj/study/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/kkj/study/springsecurity/config/SecurityConfig.java
@@ -4,15 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@Order(Ordered.LOWEST_PRECEDENCE -15)
+//@Order(Ordered.LOWEST_PRECEDENCE -15)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 계층형 권한 설정 커스터마이징
+    // Start
+    public AccessDecisionManager accessDecisionManager() {
+        // AccessDecisionManager를 커스터마이징
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+        webExpressionVoter.setExpressionHandler(handler);
+        List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
+        return new AffirmativeBased(voters);
+    }
+
+    public SecurityExpressionHandler securityExpressionHandler() {
+        // ExpressionHandler 커스터마이징
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+    }
+    // End
 
     //원칙적으로는 해줘야하는데 UserDetailsService를 구현한 구현체가 Bean으로 등록되어 있으면 안해줘도 상관없다(AccountService)
 //    @Autowired
@@ -23,8 +58,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //특정 url은 인증을 거치고 싶지 않다
         http.authorizeRequests()
                 .mvcMatchers("/", "/info", "/account/**").permitAll()
-                .mvcMatchers("/admin").hasRole(("ADMIN"))
-                .anyRequest().authenticated(); // 기타 다른 url은 인증을 해야한다.
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .mvcMatchers("/user").hasRole("USER")
+                .anyRequest().authenticated() // 기타 다른 url은 인증을 해야한다.
+                //.accessDecisionManager(accessDecisionManager());  // AccessDecisionManager를 커스터마이징
+                .expressionHandler(securityExpressionHandler());
+                // AccessDecisionManager의 voter가 사용하는 ExpressionHandler 커스터마이징
 //                .and()              //and   method chaining을 반드시 하지 않아도 됨
 //                .formLogin()        //Form Login 사용
 //                .and()
